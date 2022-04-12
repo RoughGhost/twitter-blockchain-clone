3205291861e3ce80abd1dd11839caa5409d3f49a
RoughGhost <stephendlouis6@gmail.com>
2022-04-12 22:56:43 +0100

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Sidebar from "../components/Sidebar";
+import Feed from "../components/Home/Feed";
+import Widgets from "../components/Widgets";
 const style = {
-  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  wrapper: `flex justify-center select-none bg-[#15202b] text-white`,
   content: `max-w-[1400px] w-2/3 flex justify-between `
 }
 
@@ -10,8 +12,8 @@ const Home: NextPage = () => {
      <div className={style.wrapper}>
       <div className={style.content}>
          <Sidebar />
-         <h2>feed</h2>
-         <h2>widgets</h2>
+         <Feed />
+         <Widgets />
        </div>
      </div>
   )
